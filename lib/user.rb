a1aa5b43dda71beb97763bca4e76bba2cb77df5b
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-24 13:36:06 -0800

    new lab

---
+++ lib/user.rb
@@ -0,0 +1,11 @@
+class User
+    attr_accessor :first_name, :last_name
+    # @@all = []
+    def initialize
+        
+        # @@all << self
+    end
+    # def self.all
+    #     @@all
+    # end
+end
